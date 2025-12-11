be90c60529501ccb7cc1c820436119d7a3bc3a49
maccorrales28-glitch <maccorrales28@gmail.com>
2025-12-11 13:41:28 +0100

    Delete w3schoolsv0 directory

--- w3schoolsv0/db_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'session_check.php';
-include("config_db.php");
-
-echo "Connected successfully";
-?>
--- w3schoolsv0/session_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if (!isset($_SESSION["usuario"]) || $_SESSION["usuario"] != "admin") {
-    header("Location: login.php");
-    exit;
-}
-?>
